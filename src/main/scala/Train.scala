1256312c69477adfc3101a2853a312b159af9c31
BEN SMITH <benjamin.holtzman.smith@gmail.com>
2017-12-16 20:24:33 -0500

    training concept

---
+++ src/main/scala/Train.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by benjaminsmith on 12/16/17.
+  *
+  * Can't run simulations fast enough to get an accurate prediction. Lets do two things.
+  *
+  * (1) Run a pre train period
+  * (2) Run a at-the-point-of-prediction optimization
+  *
+  */
+
+object Train {
+
+}
